d3ad9e792e7f2945cdf1b71e303d0eb5f8a5ca24
Jim Brennan <jim.j.brennan@gmail.com>
2023-02-25 13:44:27 -0500

    passed the tests

--- lib/vehicle.py
+++ lib/vehicle.py
@@ -1,2 +1,15 @@
 class Vehicle:
-    pass
+    
+    def __init__(self, wheel_size, wheel_number):
+        self.wheel_number = wheel_number
+        self.wheel_size = wheel_size
+
+
+    def go(self):
+        return("vrrrrrrrooom!")
+    
+    def fill_up_tank(self):
+        return("filling up!")
+    
+    
+
